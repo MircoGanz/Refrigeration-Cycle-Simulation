0de3a0b2acb7a0cdd168f5539f1e857b5c90d3c4
MircoGanz <135030256+MircoGanz@users.noreply.github.com>
2023-06-07 22:38:21 +0200

    Update __init__.py

--- Cycles/OneStageDX/Components/Compressor/Polynomial based Model/__init__.py
+++ Cycles/OneStageDX/Components/Compressor/Polynomial based Model/__init__.py
@@ -1,6 +1,5 @@
 from system import Component, PressureBasedComponent, MassFlowBasedComponent
 import csv
-import tkinter as tk
 from CoolProp.CoolProp import PropsSI
 from pathlib import Path
 import pandas as pd
